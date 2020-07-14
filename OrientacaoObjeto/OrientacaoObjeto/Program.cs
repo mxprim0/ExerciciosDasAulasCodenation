9d9c8419fc06bcd298399337d6047fa162988db2
dan-primo <danielly.primo@gmail.com>
2020-07-14 18:44:51 -0300

    Create Solution

---
+++ OrientacaoObjeto/OrientacaoObjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrientacaoObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
